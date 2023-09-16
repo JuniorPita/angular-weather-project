387942a07d54414dc7825ed16662418fa4b37238
Евгений Васильев <81571083+JuniorPita@users.noreply.github.com>
2023-09-16 21:33:52 +0300

    Выполнены последние изменения по созданию приложения, далее будет только модификация стилями и тд

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from 'src/models/weather.model';
+import { WeatherService } from 'src/shared/services/weather/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  public weatherData?: WeatherData;
+  public cityName: string = 'Moscow';
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  public onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData('Wellington').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      },
+    });
+  }
+}
